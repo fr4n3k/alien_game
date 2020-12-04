833a270624b396473f2a9fbfc79761f344a1c9d0
fr4n3k <franekj@gmail.com>
2020-12-04 16:42:24 +0100

    add attr to _prep_msg function call

--- button.py
+++ button.py
@@ -14,12 +14,12 @@ class Button:
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Build the button's rect objecy and center it.
+        # Build the button's rect object and center it.
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
         # The button message needs to be prepped only once.
-        self._prep_msg
+        self._prep_msg(msg)
 
     def _prep_msg(self, msg):
         """Turn msg into a rendered image and center text on the button."""
