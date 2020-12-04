2ae95829b9e590752e6fdf74cb23b337167318e3
fr4n3k <franekj@gmail.com>
2020-12-04 14:13:01 +0100

    added f: _check_fleet_edges and _change_fleet_direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,8 +101,26 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
     def _update_aliens(self):
-        """Update position of aliens in the fleet."""
+        """
+        Check if the fleet is at an edge,
+        then update position of aliens in the fleet.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_screen(self):
