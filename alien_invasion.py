a66bae11520a282d54095d0b8bccd9ac9ced87d5
fr4n3k <franekj@gmail.com>
2020-12-04 14:52:54 +0100

    refactoring _update_bullets. New f: _check_bullet_alien_collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -100,11 +100,18 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+            self._check_bullet_alien_collisions()
 
+    def _check_bullet_alien_collisions(self):
         # Check for any bullets that have hit aliens.
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
