a6637061493f05a6bdf9fe2974df53180f677f05
fr4n3k <franekj@gmail.com>
2020-12-04 09:15:01 +0100

    bullet attrs added to settings

--- settings.py
+++ settings.py
@@ -8,3 +8,8 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (80, 80, 200)
         self.ship_speed = 1
+        # Bullet settings
+        self.bullet_speed = 0.75
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
