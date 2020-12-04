85de4c454da283e2d5a33b056851e3b2730d033a
fr4n3k <franekj@gmail.com>
2020-12-04 13:43:34 +0100

    x

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 900
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (80, 80, 200)
-        self.ship_speed = 1
+        self.ship_speed = 1.5
         # Bullet settings
-        self.bullet_speed = 0.75
+        self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
