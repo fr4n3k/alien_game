f33140800332655c2eab3a5087998ad523afd74f
fr4n3k <franekj@gmail.com>
2020-12-04 15:16:21 +0100

    new f: center_ship, that centers the ship after collisions

--- ship.py
+++ ship.py
@@ -26,6 +26,11 @@ class Ship:
         self.moving_up = False
         self.moving_down = False
 
+    def center_ship(self):
+        """Center the ship on the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right and self.rect.right < self.screen_rect.right:
