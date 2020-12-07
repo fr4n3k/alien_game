e613a575be6243b386e2c19522474ed42037987c
fr4n3k <franekj@gmail.com>
2020-12-07 11:07:15 +0100

    draw scoreboard on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -214,6 +214,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
         # Draw the play button if the game is inactive
         if not self.stats.game_active:
             self.play_button.draw_button()
